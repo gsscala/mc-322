22c245eca05f1cfa73cea3f65da0a5ef8476c55d
gsscala <gsscalabr@protonmail.com>
2025-03-11 16:07:13 -0300

    added files

---
+++ lab01/Pair.java
@@ -0,0 +1,18 @@
+class Pair {
+    private int f;
+    private int s;
+
+    public Pair(int f, int s) {
+        this.f = f;
+        this.s = s;
+    }
+
+    public int first() {
+        return f;
+    }
+
+    public int second() {
+        return s;
+    }
+}
+
